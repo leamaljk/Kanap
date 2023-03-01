b73357c3755b5bb97deacab62860938eadaf856a
leamaljk <lea.zizic@hotmail.com>
2023-03-01 19:05:19 +0000

    Milestone #9 / Enabiling delete button and modifying the quantity on the cart page including in local storage

--- front/js/cart.js
+++ front/js/cart.js
@@ -14,6 +14,10 @@
             .then(response => {
             response.json().then(p => {
                 let cartItems = "";
+                // have selected cart items id from the uncomented article 
+// using the right loop and template literals we re able to list 
+// through the array depending on selected items
+
                 let cart = document.getElementById("cart__items");
                 cartItems += `<article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
               <div class="cart__item__img">
@@ -28,7 +32,9 @@
                 <div class="cart__item__content__settings">
                   <div class="cart__item__content__settings__quantity">
                     <p>Q té:</p>
-                    <input id='${p._id}-${product.color}' type="number" class="itemQuantity" onchange="updateCart('${p._id}','${product.color}')" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+                    <input id='${p._id}-${product.color}' type="number" class="itemQuantity" 
+                    onchange="updateCart('${p._id}','${product.color}')" name="itemQuantity" 
+                    min="1" max="100" value="${product.quantity}">
                   </div>
                   <div class="cart__item__content__settings__delete">
                     <p class="deleteItem" onclick="deleteItem('${p._id}','${product.color}')" >Delete</p>
@@ -47,3 +53,47 @@
         //End of foreach Loop
         //Modify the totalQuantity Element 
     });
+
+//now I am able to delete the item on the cart page by clicking on the Delete button
+//therefore deleting in local storage and in total price
+
+    const deleteItem = (id, color) => {
+      // retreive local storage array
+      let products = JSON.parse(localStorage.getItem("cart"));
+      // target id && color of item to be deleted
+      let index = products.findIndex((entry) => entry._id === id && entry.color === color);
+      if (index != -1) {
+          // remove item from array
+          products.splice(index, 1);
+          // store new array back to local storage
+          localStorage.setItem('cart', JSON.stringify(products));
+      }
+    //Update the cart
+    //clearing the cart items element
+    document.getElementById("cart__items").innerHTML = "";
+    // reload element
+    loadCart();
+};
+
+//now I am able to change the quantity on the cart page, manually
+//therefore changing in local storage and in total price
+const updateCart = (id, color) => {
+  // alert('updating');
+  //retrieve the quantity
+  let quantity = parseInt(document.getElementById(`${id}-${color}`).value);
+  // retrieve the products array
+  let products = JSON.parse(localStorage.getItem("cart"));
+  // find the index of the product
+  let index = products.findIndex((entry) => entry._id == id && entry.color == color);
+  // update the quantity
+  if (index != -1) {
+      products[index].quantity = quantity;
+      products[index].price = parseInt(products[index].uPrice) * quantity;
+  }
+  // update local storage
+  localStorage.setItem('cart', JSON.stringify(products));
+  // clear the cart display
+  document.getElementById("cart__items").innerHTML = "";
+  // reload the cart and total
+  loadCart();
+};
